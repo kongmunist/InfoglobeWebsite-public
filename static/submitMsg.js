8c63bd7174ec5728cec3b5a62e076ec2e4132b63
laharich1 <laharich1@gmail.com>
2022-09-11 00:02:56 -0400

    update history without reload, input autofocus

--- static/submitMsg.js
+++ static/submitMsg.js
@@ -5,13 +5,20 @@ function init() {
 }
 
 function handleForm(event) {
+  // prevent reload
   event.preventDefault();
+
+  // form data
   const form = document.getElementById("msg_form");
   const formData = new FormData(form)
   const msg = formData.get('msg')
   const data = JSON.stringify(msg);
-  const recents = document.getElementById("recent_msg")
 
+  // list displays
+  const recents = document.getElementById("recent_msg")
+  const history = document.getElementById("full_history")
+
+  // send request
   fetch('/savemessage', {
     headers: {
       'Content-Type': 'application/json'
@@ -20,10 +27,18 @@ function handleForm(event) {
     body: data,
   })
 
+  // update history
+  const span = document.createElement("span")
+  span.innerHTML = recents.lastElementChild.innerHTML
+  const br = document.createElement("br")
+  history.prepend(span, br)
+
+  // update recents
   const li = document.createElement("li")
   li.innerHTML = msg
   recents.prepend(li)
   recents.lastElementChild.remove()
+
   form.reset()
 }
 
